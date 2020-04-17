71a93c6f078a85ca135737100663a36eebd9e25a
Carlos <miklo.concepcion@gmail.com>
2020-04-16 23:21:28 -0500

    updating handleRangeFunction errors on my portion of the task

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,7 +2,7 @@
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress_filled");
+const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
@@ -38,7 +38,6 @@ function scrub(e) {
 }
 
 // hooking up the event listeners
-
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
